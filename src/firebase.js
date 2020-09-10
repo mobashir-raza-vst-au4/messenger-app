45eccb584022aa16be46907c6f3977b895e6dc1e
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-09-10 20:42:55 +0530

    firebase change

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebases = firebase.initializeApp({
-    apiKey: "AIzaSyBGyAL92eF1jhyYBggfyPU6UaGywwEfP4M",
-    authDomain: "messenger-clone-1bc0a.firebaseapp.com",
-    databaseURL: "https://messenger-clone-1bc0a.firebaseio.com",
-    projectId: "messenger-clone-1bc0a",
-    storageBucket: "messenger-clone-1bc0a.appspot.com",
-    messagingSenderId: "639360578666",
-    appId: "1:639360578666:web:277ef48b66c5b974a60613",
-    measurementId: "G-GDSQX9T9ZV"
+    apiKey: "AIzaSyDMqKMrI96AjOkMc3sTlNW_jF1b60F6idc",
+    authDomain: "messenger-app-clone-f83ec.firebaseapp.com",
+    databaseURL: "https://messenger-app-clone-f83ec.firebaseio.com",
+    projectId: "messenger-app-clone-f83ec",
+    storageBucket: "messenger-app-clone-f83ec.appspot.com",
+    messagingSenderId: "1064672450566",
+    appId: "1:1064672450566:web:87665cfeae97e686a48b87",
+    measurementId: "G-VYCE43HFHS"
 });
 
 const db = firebases.firestore();
