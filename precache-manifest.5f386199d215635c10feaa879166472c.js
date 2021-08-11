9a4c910023482c299223b772f251eac4a45877f9
Md Mobashir Raza <mobashir.raza@nettantra.net>
2021-08-11 18:29:32 +0530

    Updates

---
+++ precache-manifest.5f386199d215635c10feaa879166472c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5fa990a6ffa70e850a29e30271a853",
+    "url": "/messenger-app/index.html"
+  },
+  {
+    "revision": "938101c2844045f18f6b",
+    "url": "/messenger-app/static/css/main.1dc5c07c.chunk.css"
+  },
+  {
+    "revision": "72311ff0ec3bec2a8132",
+    "url": "/messenger-app/static/js/2.cb52bbe2.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger-app/static/js/2.cb52bbe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938101c2844045f18f6b",
+    "url": "/messenger-app/static/js/main.e7623c86.chunk.js"
+  },
+  {
+    "revision": "0ab553d702eac7b353d2",
+    "url": "/messenger-app/static/js/runtime-main.4661fb3e.js"
+  }
+]);
